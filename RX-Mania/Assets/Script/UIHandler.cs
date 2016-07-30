940fa2cca8c34123fe870dc8e4f0b4d84b08f1b4
Jonathan Thomas <jthom252@gmail.com>
2016-07-29 23:32:27 -0700

    Added additional art

--- RX-Mania/Assets/Script/UIHandler.cs
+++ RX-Mania/Assets/Script/UIHandler.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace RX {
-    class UIHandler {
+    class UIHandler : MonoBehaviour {
+        public Color[] backgroundColors; 
+        public float backgroundScrollSpeedPerFrame = 0.1f;
+        public bool scrollLeft = true; 
+        public bool scrollUp = true;
+        public MeshRenderer background; 
 
-        public void IterateBackgroundColor() {
+        public void Update() {
+            IterateBackground(); 
+        }//Update
+
+        public void IterateBackground() {
             //Use Lerp between a handful of predefined colors
+            background.material.mainTextureOffset += new Vector2(
+                scrollLeft ? backgroundScrollSpeedPerFrame : -backgroundScrollSpeedPerFrame, 
+                scrollUp ? -backgroundScrollSpeedPerFrame : backgroundScrollSpeedPerFrame);
         }//IterateBackgroundColor
     }//UIHandler
 }//RX
